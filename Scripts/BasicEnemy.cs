48fa4442061eca290401d7e7e69f1fbeec05158f
Gabriel Steele <93623975+MikeVey42@users.noreply.github.com>
2024-08-04 22:12:52 -0400

    enemies shoot sword slash projectiles

--- Scripts/BasicEnemy.cs
+++ Scripts/BasicEnemy.cs
@@ -1,17 +1,26 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class BasicEnemy : CharacterBody2D
 {
 	Player player;
+	PackedScene swordSlashPrefab;
 
 	private float speed = 200;
+
+	Timer attackTimer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		// Find the player in the scene
 		player = GetParent().GetNode<Player>("Player");
+		// Load the sword slash prefab
+		swordSlashPrefab = GD.Load<PackedScene>("res://Scenes/SwordSlash.tscn");
+		attackTimer = new Timer();
+		attackTimer.Start(1);
+		attackTimer.Timeout += fire;
 	}
 
     public override void _PhysicsProcess(double delta)
@@ -21,4 +30,14 @@ public partial class BasicEnemy : CharacterBody2D
 		Velocity = directionToPlayer.Normalized() * speed;
 		MoveAndSlide();
     }
+
+	public void fire() {
+		if ((player.Position - this.Position).Length() < 150) {
+			SwordSlash swordSlash = (SwordSlash) swordSlashPrefab.Instantiate();
+			Debug.Print("fired!");
+			GetParent().AddChild(swordSlash);
+			swordSlash.Position = this.Position;
+			swordSlash.Rotation = this.Rotation;
+		}
+	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,22 +1,29 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Player : CharacterBody2D
 {
 
-	int speed = 400;  // move speed in pixels/sec
+    int speed = 400;  // move speed in pixels/sec
+    private int health;
 
     public override void _Ready()
     {
-
+        health = 20;
     }
 
     public override void _PhysicsProcess(double delta)
     {
-		LookAt(GetGlobalMousePosition());
-		float yVelocity = Input.GetAxis("up", "down");
-		float xVelocity = Input.GetAxis("left", "right");
-		Velocity = new Vector2(xVelocity, yVelocity) * speed;
-		MoveAndSlide();
+        LookAt(GetGlobalMousePosition());
+        float yVelocity = Input.GetAxis("up", "down");
+        float xVelocity = Input.GetAxis("left", "right");
+        Velocity = new Vector2(xVelocity, yVelocity) * speed;
+        MoveAndSlide();
+    }
+
+    public void Damage(int amount) {
+        health -= amount;
+        Debug.Print("New health is: " + health);
     }
 }
